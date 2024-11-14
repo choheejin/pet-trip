da53bf0a6b6221211b3ddea834a6845afb29492f
조희진 <allana123@naver.com>
2024-11-14 17:46:14 +0900

    feat(Map): 지도 검색 화면 완료

--- pet/src/main/java/com/ssafy/pet/config/WebConfig.java
+++ pet/src/main/java/com/ssafy/pet/config/WebConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,5 +27,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(confirmInterceptor).addPathPatterns(patterns);
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")	//모든 요청에 대해
+		.allowedOriginPatterns("*")	//모든 오리진에 적용
+		.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+		.allowCredentials(true);
 	}
 }
