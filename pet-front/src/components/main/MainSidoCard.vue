d37d6aa242018223dad3206fd6a2f002fd23f402
조희진 <allana123@naver.com>
2024-11-20 12:25:22 +0900

    feat(map): 경로 선택시 자동 줌인 줌아웃

--- pet-front/src/components/main/MainSidoCard.vue
+++ pet-front/src/components/main/MainSidoCard.vue
@@ -3,26 +3,10 @@ import { ref, computed } from "vue";
 import { useMainSelectStore } from "@/stores/mainselect.js";
 
 import router from "@/router/index.js";
+import { useSidoStore } from "@/stores/sido";
 
-const images = [
-  { sido_name: "서울", sido_code: 1 },
-  { sido_name: "인천", sido_code: 2 },
-  { sido_name: "대전", sido_code: 3 },
-  { sido_name: "대구", sido_code: 4 },
-  { sido_name: "광주", sido_code: 5 },
-  { sido_name: "부산", sido_code: 6 },
-  { sido_name: "울산", sido_code: 7 },
-  { sido_name: "세종특별자치시", sido_code: 8 },
-  { sido_name: "경기도", sido_code: 31 },
-  { sido_name: "강원특별자치도", sido_code: 32 },
-  { sido_name: "충청북도", sido_code: 33 },
-  { sido_name: "충청남도", sido_code: 34 },
-  { sido_name: "경상북도", sido_code: 35 },
-  { sido_name: "경상남도", sido_code: 36 },
-  { sido_name: "전북특별자치도", sido_code: 37 },
-  { sido_name: "전라남도", sido_code: 38 },
-  { sido_name: "제주도", sido_code: 39 },
-];
+const sidoStore = useSidoStore();
+const images = sidoStore.sido;
 
 const mainSelectStore = useMainSelectStore();
 const currentIndex = ref(0);
