62c695051c674c7cd11c59061f63a936ef925ae7
choheejin <hjcho9510@gmail.com>
2024-11-24 17:59:01 +0900

    feat(map): 여행지 좋아요 조회, 등록, 취소 완료

--- pet-front/src/api/attractionApi.js
+++ pet-front/src/api/attractionApi.js
@@ -1,7 +1,19 @@
+import { useAuthStore } from "@/stores/user";
 import axios from "axios";
 
 const attractionApi = axios.create({
   baseURL: "http://localhost:8080/pet/attraction",
+});
+
+attractionApi.interceptors.request.use((config) => {
+  const authStore = useAuthStore();
+  if (config.url == "/openapi-detail" && authStore.token) {
+    config.headers.accessToken = authStore.token;
+  }
+  if (config.method === "post" || config.method === "delete") {
+    config.headers.accessToken = authStore.token;
+  }
+  return config;
 });
 
 export default attractionApi;
